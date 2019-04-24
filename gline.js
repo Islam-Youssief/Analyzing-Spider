013ad8d27963702e76e4816731885adf3c633a3c
Islam Youssief <Islam.youssief@gmail.com>
2019-04-24 19:10:35 +0200

    Doing words analysis

---
+++ gline.js
@@ -0,0 +1,3 @@
+gline = [ ['Month','umich.edu','mac.com','unl.edu','mtu.edu','berkeley.edu','ucdavis.edu','columbia.edu','indiana.edu','ufp.pt','virginia.edu'],
+['2005-12',46,12,12,11,11,10,9,9,9,8]
+];
